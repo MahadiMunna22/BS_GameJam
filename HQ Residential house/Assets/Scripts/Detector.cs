ff5f1ae5f303dadd7f88dc6f61ec9e167c1df498
Zobair hasan <76781619+Zobair1234@users.noreply.github.com>
2021-12-27 15:40:32 +0600

    AI detection complete

--- HQ Residential house/Assets/Scripts/Detector.cs
+++ HQ Residential house/Assets/Scripts/Detector.cs
@@ -22,7 +22,7 @@ public class Detector : MonoBehaviour
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10, Color.red);
         if (Physics.Raycast(landingRay, out hit, 1000))
         {
-            Debug.Log("asd");
+            //Debug.Log("asd");
 
             if (hit.collider.tag == "Pickable")
             {
@@ -32,7 +32,7 @@ public class Detector : MonoBehaviour
                 //Interact.SetActive(true);
                 if(Input.GetKeyDown(KeyCode.F))
                 {
-                    Debug.Log(hit.transform.gameObject.name);
+                    //Debug.Log(hit.transform.gameObject.name);
                    interceptedObj = hit.transform.gameObject;
                   
                    Inventory.Instance.Additems(interceptedObj); 
