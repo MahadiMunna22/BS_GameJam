e6c4fd07c483fa8ff5ab97230067f8acfd14325b
Mahadi Munna <mahadimunna22@gmail.com>
2021-12-28 20:42:28 +0600

    Dialog added

---
+++ HQ Residential house/Assets/DialogManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string[] dialogs = new string[]{"Hello brother, please help me", "That man living in that house took away my properties", "Now I have nothing and I am homeless", "I heard you help the homeless poor people", "So I called you", "Please help me"};
+
+    public string[] endDialogs = { "You are a hero", "That person deserved it and I can also start a new life from here", "Thank you so much" };
+
+    public TMPro.TextMeshProUGUI text; 
+    public Button LoadNext;
+    private int dialogNum = 0;
+    private int endDialogNum = 0;
+    public bool stealingDone;
+    public GameObject dialogPanel;
+
+    private void Start()
+    {
+        dialogNum = 0;
+        endDialogNum = 0;
+        if (stealingDone)
+        {
+            text.text = endDialogs[endDialogNum];
+        }
+        else
+        {
+            text.text = dialogs[dialogNum];
+        }
+
+        Button btn = LoadNext.GetComponent<Button>();
+        btn.onClick.AddListener(LoadNextDialog);
+    }
+    private void Update()
+    {
+        
+        
+    }
+
+    public void LoadNextDialog()
+    {
+        if (stealingDone)
+        {
+            if (endDialogNum < endDialogs.Length - 1)
+            {
+                endDialogNum += 1;
+                text.text = endDialogs[endDialogNum];
+            }
+            else
+            {
+                dialogPanel.SetActive(false);
+            }
+        }
+        else
+        {
+            if (dialogNum < dialogs.Length - 1)
+            {
+                dialogNum += 1;
+                text.text = dialogs[dialogNum];
+            }
+            else
+            {
+                dialogPanel.SetActive(false);
+            }
+        }
+        
+    }
+}
--- HQ Residential house/Assets/Scripts/MainMenu.cs
+++ HQ Residential house/Assets/Scripts/MainMenu.cs
@@ -8,12 +8,17 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject menu;
     public void PlayGame(){
+<<<<<<< Updated upstream
         /* SceneManager.LoadScene(1);*/
          Cursor.lockState = CursorLockMode.Locked;
          Cursor.visible = false;
          menu.SetActive(false);
         
 }
+=======
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+>>>>>>> Stashed changes
 
     public void QuitGame(){
         Application.Quit();
